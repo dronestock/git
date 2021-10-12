0cd38d250368c42f28bf6a3522c92f8ea8dce401
storezhang <storezhang@gmail.com>
2021-10-12 09:23:58 +0800

    项目初始化

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM alpine
+
+MAINTAINER storezhang "storezhang@gmail.com"
+LABEL architecture="AMD64/x86_64" version="latest" build="2021-06-04"
+LABEL Description="Drone持续集成Git插件，增加标签功能。"
+
+
+
+# 复制文件
+COPY git.sh /bin
+
+
+
+ENTRYPOINT /bin/git.sh
---
+++ git.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# 清空本地缓存
+if [ "${PLUGIN_CLEAR}" = true ] ; then
+  rm -rf .git
+fi
+
+# 写入授权文件，在使用过程中无需输入密码
+mkdir -pv "${HOME}"/.ssh
+echo "${PLUGIN_SSH_KEY}" > "${HOME}"/.ssh/id_rsa
+chmod 400 "${HOME}"/.ssh/id_rsa
+
+
+cd "${PLUGIN_PATH}" || exit
+
+# 初始化Git仓库
+export GIT_SSH_COMMAND='ssh -o StrictHostKeyChecking=no'
+git config --global user.name "${DRONE_COMMIT_AUTHOR}"
+git config --global user.email "${DRONE_COMMIT_AUTHOR_EMAIL}"
+git init
+
+# 将当前目录添加到仓库中
+git add .
+[ -z "${PLUGIN_COMMIT_MESSAGE}" ] && PLUGIN_COMMIT_MESSAGE='push'
+[ "${PLUGIN_FORCE}" = "true" ] && PLUGIN_FORCE='--force'
+git commit . -m "${PLUGIN_COMMIT_MESSAGE}"
+
+# 打标签
+if [ -n "${PLUGIN_TAG}" ]; then
+  git tag -a "${PLUGIN_TAG}" -m ${PLUGIN_COMMIT_MESSAGE}
+fi
+
+# 添加远程服务器并推送代码
+git remote add origin "${PLUGIN_REMOTE}"
+git push --set-upstream origin master ${PLUGIN_FORCE}
