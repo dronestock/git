5c517311a175a22cc9a7116bfff18323b8cbe227
Storezhang <storezhang@gmail.com>
2021-12-31 23:18:26 +0800

    增加拉取代码模式

--- ssh.go
+++ ssh.go
@@ -52,7 +52,7 @@ func writeSSHKey(home string, key string, logger simaqian.Logger) (err error) {
 }
 
 func writeSSHConfig(home string, logger simaqian.Logger) (err error) {
-	configFile := filepath.Join(home, `configFile`)
+	configFile := filepath.Join(home, `config`)
 	keyfileField := field.String(`config.file`, configFile)
 	if err = ioutil.WriteFile(configFile, []byte(sshConfig), 0400); nil != err {
 		logger.Error(`写入SSH配置文件出错`, keyfileField, field.Error(err))
