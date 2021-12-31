d12b95764e9698f1e1adc411b596ca86bfde6e39
Storezhang <storezhang@gmail.com>
2021-12-31 21:31:02 +0800

    增加拉取代码模式

--- config.go
+++ config.go
@@ -45,6 +45,7 @@ func (c *config) Fields() gox.Fields {
 	return []gox.Field{
 		field.String(`remote`, c.Remote),
 		field.String(`folder`, c.Folder),
+		field.Strings(`ssh.key`, c.SSHKey),
 		field.String(`branch`, c.Branch),
 		field.String(`tag`, c.Tag),
 		field.String(`author`, c.Author),
