fbd5a4b71c8c7fb2caebc1a6e49e13b28540779a
Storezhang <storezhang@gmail.com>
2022-01-15 21:27:31 +0800

    调整Fastgithub日志级别

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,33 @@
-FROM storezhang/alpine AS builder
+FROM ccr.ccs.tencentyun.com/storezhang/alpine AS fastgithub
 
 
 # Github加速版本
 ENV FAST_GITHUB_VERSION 2.1.2
+WORKDIR /opt
 
 
 RUN apk add unzip
-RUN wget https://ghproxy.com/https://github.com/dotnetcore/FastGithub/releases/download/${FAST_GITHUB_VERSION}/fastgithub_linux-x64.zip --output-document /fastgithub_linux-x64.zip
+RUN wget https://ghproxy.com/https://github.com/dotnetcore/FastGithub/releases/download/${FAST_GITHUB_VERSION}/fastgithub_linux-x64.zip --output-document fastgithub_linux-x64.zip
 RUN unzip fastgithub_linux-x64.zip
-RUN mv /fastgithub_linux-x64 /opt/fastgithub
+RUN mv fastgithub_linux-x64 /opt/fastgithub
 RUN chmod +x /opt/fastgithub/fastgithub
 
 
 
 # 打包真正的镜像
-FROM storezhang/alpine
+FROM ccr.ccs.tencentyun.com/storezhang/alpine
 
 
-MAINTAINER storezhang "storezhang@gmail.com"
-LABEL architecture="AMD64/x86_64" version="latest" build="2021-12-31"
-LABEL Description="Drone持续集成Git插件，增加标签功能以及Github加速功能。同时支持推拉模式"
+LABEL author="storezhang<华寅>"
+LABEL email="storezhang@gmail.com"
+LABEL qq="160290688"
+LABEL wechat="storezhang"
+LABEL description="Drone持续集成Git插件，增加标签功能以及Github加速功能。同时支持推拉模式"
 
 
 # 复制文件
-COPY --from=builder /opt/fastgithub /opt/fastgithub
+COPY --from=fastgithub /opt/fastgithub /opt/fastgithub
+COPY docker /
 COPY git /bin
 
 
--- github.go
+++ github.go
@@ -4,6 +4,7 @@ import (
 	`time`
 
 	`github.com/storezhang/gex`
+	`github.com/storezhang/gox`
 	`github.com/storezhang/gox/field`
 	`github.com/storezhang/simaqian`
 )
@@ -13,13 +14,18 @@ func github(conf *config, logger simaqian.Logger) (err error) {
 		return
 	}
 
-	logger.Info(`开始启动Github加速`, conf.Fields()...)
-	options := gex.NewOptions(gex.ContainsChecker(`FastGithub启动完成`), gex.Async())
-	if !conf.Verbose {
-		options = append(options, gex.ClearTerminal())
+	// 记录日志
+	fields := gox.Fields{
+		field.String(`exe`, conf.fastgithubExe),
+		field.String(`success.mark`, conf.fastgithubSuccessMark),
 	}
-	if _, err = gex.Run(`/opt/fastgithub/fastgithub`, options...); nil != err {
-		logger.Error(`Github加速出错`, conf.Fields().Connect(field.Error(err))...)
+	logger.Info(`开始启动Github加速`, fields...)
+	options := gex.NewOptions(gex.ContainsChecker(conf.fastgithubSuccessMark), gex.Async())
+	if !conf.Verbose {
+		options = append(options, gex.Quiet())
+	}
+	if _, err = gex.Run(conf.fastgithubExe, options...); nil != err {
+		logger.Error(`Github加速出错`, fields.Connect(field.Error(err))...)
 	}
 	if nil != err {
 		return
@@ -32,9 +38,10 @@ func github(conf *config, logger simaqian.Logger) (err error) {
 		newEnv(`FTP_PROXY`, proxy),
 		newEnv(`NO_PROXY`, `localhost, 127.0.0.1, ::1`),
 	)
+
 	// 尽量避免刚启动完成就使用代理而出现Connection refused
 	time.Sleep(time.Second)
-	logger.Info(`Github加速成功`, conf.Fields()...)
+	logger.Info(`Github加速成功`, fields...)
 
 	return
 }
