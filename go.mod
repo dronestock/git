cdf5bd27a57b43ac54b6f520c8cab9b1e37285a9
storezhang <storezhang@gmail.com>
2022-01-19 13:24:13 +0800

    修复日志显示错误

--- go.mod
+++ go.mod
@@ -3,11 +3,12 @@ module github.com/dronestock/git
 go 1.17
 
 require (
+	github.com/dronestock/drone v0.0.2
 	github.com/storezhang/gex v0.1.0
 	github.com/storezhang/gfx v0.0.2
 	github.com/storezhang/gox v1.8.3
 	github.com/storezhang/mengpo v0.1.0
-	github.com/storezhang/simaqian v0.1.0
+	github.com/storezhang/simaqian v0.1.1
 	github.com/storezhang/validatorx v1.0.9
 )
 
@@ -22,7 +23,7 @@ require (
 	github.com/storezhang/guc v0.0.4 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/zap v1.19.1 // indirect
+	go.uber.org/zap v1.20.0 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/sys v0.0.0-20211031064116-611d5d643895 // indirect
 	golang.org/x/text v0.3.3 // indirect
---
+++ plugin.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	`github.com/dronestock/drone`
+)
+
+type plugin struct {
+	config *config
+
+	envs                  []string
+	fastGithubExe         string
+	fastGithubSuccessMark string
+	gitExe                string
+}
+
+func newPlugin() drone.Plugin {
+	return &plugin{
+		config: new(config),
+
+		envs:                  make([]string, 0),
+		fastGithubExe:         `/opt/fastgithub/fastgithub`,
+		fastGithubSuccessMark: `FastGithub启动完成`,
+		gitExe:                `git`,
+	}
+}
+
+func (p *plugin) Configuration() drone.Configuration {
+	return p.config
+}
+
+func (p *plugin) Steps() []*drone.Step {
+	return []*drone.Step{
+		drone.NewStep(p.github, drone.Name(`Github加速`)),
+		drone.NewStep(p.clear, drone.Name(`清理Git目录`)),
+		drone.NewStep(p.ssh, drone.Name(`写入SSH配置`)),
+		drone.NewStep(p.pull, drone.Name(`拉代码`)),
+		drone.NewStep(p.push, drone.Name(`推代码`)),
+	}
+}
