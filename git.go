f44623dddedc7066974e6b5cd8b7f9d0ac7ec336
Storezhang <storezhang@gmail.com>
2022-02-09 20:09:57 +0800

    升级依赖

--- git.go
+++ git.go
@@ -4,6 +4,6 @@ import (
 	`github.com/dronestock/drone`
 )
 
-func (p *plugin) git(args ...string) error {
+func (p *plugin) git(args ...interface{}) error {
 	return p.Exec(gitExe, drone.Args(args...), drone.Dir(p.Folder), drone.Envs(p.envs...))
 }
