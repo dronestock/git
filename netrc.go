3f4e9438b113c1ede7648d06ab6e97c14c962613
storezhang <storezhang@gmail.com>
2022-03-10 17:30:03 +0800

    增加授权文件写入

--- netrc.go
+++ netrc.go
@@ -3,7 +3,6 @@ package main
 import (
 	`fmt`
 	`io/ioutil`
-	`net/url`
 	`os`
 	`path/filepath`
 	`strings`
@@ -12,7 +11,8 @@ import (
 	`github.com/storezhang/gox/field`
 )
 
-const netrcConfigFormatter = `machine %s
+const netrcConfigFormatter = `
+machine %s
 login %s
 password %s
 `
@@ -22,21 +22,10 @@ func (p *plugin) netrc() (undo bool, err error) {
 		return
 	}
 
-	var host string
-	if remote, parseErr := url.Parse(p.remote()); nil != parseErr {
-		err = parseErr
-	} else {
-		host = remote.Host
-	}
-	if nil != err {
-		return
-	}
-
 	netrcFilepath := filepath.Join(os.Getenv(homeEnv), netrcFilename)
-	netrcConfig := fmt.Sprintf(netrcConfigFormatter, host, p.Username, p.Password)
-	fmt.Println(netrcConfig)
+	netrcConfig := fmt.Sprintf(netrcConfigFormatter, p.Machine, p.Username, p.Password)
 	netrcFields := gox.Fields{
-		field.String(`host`, host),
+		field.String(`machine`, p.Machine),
 		field.String(`username`, p.Username),
 	}
 	if err = ioutil.WriteFile(netrcFilepath, []byte(netrcConfig), defaultFilePerm); nil != err {
