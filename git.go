5b316a8c8bc258f4679e37620533564a8d6857e1
storezhang <storezhang@gmail.com>
2022-10-26 13:41:13 +0800

    feat(Hosts): 改成配置目录

--- git.go
+++ git.go
@@ -8,7 +8,7 @@ func (p *plugin) git(args ...any) error {
 	return p.Exec(
 		gitExe,
 		drone.Args(args...),
-		drone.Dir(p.Folder),
+		drone.Dir(p.Dir),
 		drone.Env(`GIT_HTTP_LOW_SPEED_LIMIT`, `1024`),
 		drone.Env(`GIT_HTTP_LOW_SPEED_TIME`, `60`),
 		drone.StringEnvs(p.envs...),
