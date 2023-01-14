1a82843d5b03f5bf4d71a10f0598cb82b02a3911
storezhang <storezhang@gmail.com>
2023-01-14 21:51:01 +0800

    fix(Git): 有netrc配置文件时，删除后再写入

--- netrc.go
+++ netrc.go
@@ -13,17 +13,25 @@ import (
 const netrcConfigFormatter = "default login %s password %s"
 
 func (p *plugin) netrc() (undo bool, err error) {
-	if undo = "" == strings.TrimSpace(p.Username) || `` == strings.TrimSpace(p.Password); undo {
+	if undo = "" == strings.TrimSpace(p.Username) || "" == strings.TrimSpace(p.Password); undo {
 		return
 	}
 
 	netrcFilepath := filepath.Join(os.Getenv(homeEnv), netrcFilename)
+	if _, se := os.Stat(netrcFilepath); nil != se && os.IsExist(se) {
+		_ = os.Remove(netrcFilepath)
+	}
+
 	netrcConfig := fmt.Sprintf(netrcConfigFormatter, p.Username, p.Password)
 	netrcFields := gox.Fields[any]{
+		field.New("filepath", netrcFilepath),
 		field.New("username", p.Username),
+		field.New("password", p.Password),
 	}
 	if err = os.WriteFile(netrcFilepath, []byte(netrcConfig), defaultFilePerm); nil != err {
 		p.Error("写入授权文件出错", netrcFields.Connect(field.Error(err))...)
+	} else {
+		p.Debug("写入授权文件成功", netrcFields...)
 	}
 
 	return
