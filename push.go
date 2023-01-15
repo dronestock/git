bee2d7535cfdbf0e4a38298df763689a78a50760
storezhang <storezhang@gmail.com>
2023-01-15 18:43:38 +0800

    fix(Git): 推送时，先判断是否已经是Git目录，如果不是才做Git初始化和配置

--- push.go
+++ push.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/goexl/gox/rand"
+	"github.com/goexl/gox/field"
 )
 
 func (p *plugin) push() (undo bool, err error) {
@@ -9,23 +9,12 @@ func (p *plugin) push() (undo bool, err error) {
 		return
 	}
 
-	// 设置默认分支
-	if err = p.git("config", "--global", "init.defaultBranch", "master"); nil != err {
-		return
+	if se := p.git("status"); nil != se {
+		err = p.config()
+	} else {
+		p.Debug("是完整的Git仓库，无需初始化和配置", field.New("dir", p.Dir))
 	}
-
-	// 设置用户名
-	if err = p.git("config", "--global", "user.name", p.Author); nil != err {
-		return
-	}
-
-	// 设置邮箱
-	if err = p.git("config", "--global", "user.email", p.Email); nil != err {
-		return
-	}
-
-	// 初始化目录
-	if err = p.git("init"); nil != err {
+	if nil != err {
 		return
 	}
 
@@ -39,24 +28,18 @@ func (p *plugin) push() (undo bool, err error) {
 		return
 	}
 
-	name:=rand.New().String().Generate()
-	// 添加远程仓库地址
-	if err = p.git("remote", "add", name, p.remote()); nil != err {
-		return
-	}
-
 	// 如果有标签，推送标签
 	if "" != p.Tag {
 		if err = p.git("tag", "--annotate", p.Tag, "--message", p.Message); nil != err {
 			return
 		}
-		if err = p.git("push", "--set-upstream", name, p.Tag, p.gitForce()); nil != err {
+		if err = p.git("push", "--set-upstream", "original", p.Tag, p.gitForce()); nil != err {
 			return
 		}
 	}
 
 	// 推送
-	if err = p.git("push", "--set-upstream", name, p.Branch, p.gitForce()); nil != err {
+	if err = p.git("push", "--set-upstream", "original", p.Branch, p.gitForce()); nil != err {
 		return
 	}
 
