e662ed2e7674f5986b3edf6f92eead8f4dc08cbf
storezhang <storezhang@gmail.com>
2023-01-30 17:29:23 +0800

    fix(Git): 只推送改变的文件

--- commit.go
+++ commit.go
@@ -1,6 +1,11 @@
 package main
 
 func (p *plugin) commit() (err error) {
+	// 签出目标分支
+	if err = p.git("checkout", "-B", p.Branch);nil!=err{
+		return
+	}
+
 	// 只添加改变的文件
 	if err = p.git("add", "."); nil != err {
 		return
