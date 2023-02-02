3334c5b15f25109326d23d156a4159fd0c586da1
storezhang <storezhang@gmail.com>
2023-02-02 12:41:37 +0800

    fix(Git): 修复非Git项目时，提交不了代码的问题

--- commit.go
+++ commit.go
@@ -1,18 +1,20 @@
 package main
 
-func (p *plugin) commit() (err error) {
-	// 签出目标分支
-	if err = p.git("checkout", "-B", p.Branch);nil!=err{
-		return
-	}
+import (
+	"github.com/goexl/gox/field"
+)
 
+func (p *plugin) commit() (err error) {
+	p.Debug("提交代码开始", field.New("dir", p.Dir))
 	// 只添加改变的文件
 	if err = p.git("add", "."); nil != err {
 		return
 	}
 
 	// 提交
-	err = p.git("commit", ".", "--message", p.Message)
+	if err = p.git("commit", ".", "--message", p.Message);nil==err{
+		p.Debug("提交代码完成", field.New("dir", p.Dir))
+	}
 
 	return
 }
