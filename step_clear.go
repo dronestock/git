0c7e1fd6d6a07435f5f8137edfb82fd8a90eea6f
storezhang <storezhang@gmail.com>
2023-02-03 11:52:43 +0800

    fix(Git): 修复判断是否为Git目录的方式

--- step_clear.go
+++ step_clear.go
@@ -3,10 +3,7 @@ package main
 import (
 	"context"
 	"os"
-	"path"
 	"path/filepath"
-
-	"github.com/goexl/gfx"
 )
 
 type stepClear struct {
@@ -24,26 +21,5 @@ func (s *stepClear) Runnable() bool {
 }
 
 func (s *stepClear) Run(_ context.Context) (err error) {
-	// 删除Git目录，防止重新提交时，和原来用户非同一个人
-	gitFolder := filepath.Join(s.Dir, ".git")
-	if _, exists := gfx.Exists(gitFolder); exists {
-		err = s.remove(gitFolder)
-	}
-
-	return
-}
-func (s *stepClear) remove(dir string) (err error) {
-	var fis []os.DirEntry
-	if fis, err = os.ReadDir(dir); nil != err {
-		return
-	}
-
-	// 删除所有
-	for _, fi := range fis {
-		if err = os.RemoveAll(path.Join(dir, fi.Name())); nil != err {
-			return
-		}
-	}
-
-	return
+	return os.RemoveAll(filepath.Join(s.Dir, gitHome))
 }
