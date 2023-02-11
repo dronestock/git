f86d823e0921f0b80523bc9e19aff0cdd3b81495
storezhang <storezhang@gmail.com>
2023-02-11 17:56:20 +0800

    fix(Git): 修复初始化目录时，未初始化就开始设置参数

--- init.go
+++ init.go
@@ -1,6 +1,11 @@
 package main
 
 func (p *plugin) init() (err error) {
+	// 初始化目录
+	if err = p.git("init"); nil != err {
+		return
+	}
+
 	// 设置默认分支
 	if err = p.git("config", "init.defaultBranch", "master"); nil != err {
 		return
@@ -17,12 +22,7 @@ func (p *plugin) init() (err error) {
 	}
 
 	// 设置不强击检查换行符
-	if err = p.git("config", "core.autocrlf", "false"); nil != err {
-		return
-	}
-
-	// 初始化目录
-	err = p.git("init")
+	err = p.git("config", "core.autocrlf", "false")
 
 	return
 }
